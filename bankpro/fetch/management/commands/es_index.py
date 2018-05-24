e3849f9fd5fdab8cd70447ac2673808b33ea934b
gambhirpulkit <gambhirpulkit@gmail.com>
2018-05-25 01:06:00 +0530

    removed ES

--- bankpro/fetch/management/commands/es_index.py
+++ bankpro/fetch/management/commands/es_index.py
@@ -1,32 +1,28 @@
 from django.core.management.base import BaseCommand
-from fetch.models import Banks, Branches
-from elasticsearch import Elasticsearch
-import json
-import requests
 
 class Command(BaseCommand):
     help = 'ES indexes'
 
     def handle(self, *args, **options):
         print("start")
-        self.wakeup()
+        # self.wakeup()
 
 
-    def wakeup(self):
-        es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-
-        branches = Branches.objects.filter()
-        if branches.exists():
-            for branch in branches:
-                context = {}
-                context["branch_name"] = branch.branch
-                context["city"] = branch.city
-                context["bank_id"] = branch.bank.id
-                context["ifsc"] = branch.ifsc
-                context["address"] = branch.address
-                context["district"] = branch.district
-                context["state"] = branch.state
-                context["bank_name"] = branch.bank.name
-                es.index(index='branch', doc_type='bank', id=branch.ifsc, body=json.dumps(context))
-                print(es.get(index='branch', doc_type='bank', id=branch.ifsc))
-        print("end")
+    # def wakeup(self):
+    #     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    #
+    #     branches = Branches.objects.filter()
+    #     if branches.exists():
+    #         for branch in branches:
+    #             context = {}
+    #             context["branch_name"] = branch.branch
+    #             context["city"] = branch.city
+    #             context["bank_id"] = branch.bank.id
+    #             context["ifsc"] = branch.ifsc
+    #             context["address"] = branch.address
+    #             context["district"] = branch.district
+    #             context["state"] = branch.state
+    #             context["bank_name"] = branch.bank.name
+    #             es.index(index='branch', doc_type='bank', id=branch.ifsc, body=json.dumps(context))
+    #             print(es.get(index='branch', doc_type='bank', id=branch.ifsc))
+    #     print("end")
--- bankpro/fetch/views.py
+++ bankpro/fetch/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from fetch.models import Banks, Branches
 import json
-from elasticsearch import Elasticsearch
-
 
 def branch_details(request):
 
@@ -10,49 +8,38 @@ def branch_details(request):
         ifsc = request.GET.get('ifsc')
         bank_name = request.GET.get('name')
         bank_city = request.GET.get('city')
-        es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'size': 1000}])
-        if ifsc:
-            # branch_detail = es.search(index="branch", body={"query": {"match": {'ifsc': ifsc}}})
-            branch_detail = es.search(index="branch", body={
-                "query": {"bool": {"must": [{"term": {"ifsc": ifsc.lower()}}]}}
-            })
 
-            branch_detail = branch_detail['hits']['hits']
-            # branch_detail = Branches.objects.filter(ifsc__iexact=ifsc)
-            if branch_detail:
-                branch_detail = branch_detail[0]['_source']
+        if ifsc:
+
+            branch_detail = Branches.objects.filter(ifsc__iexact=ifsc)
+            if branch_detail.exists():
                 context = {}
-                context["branch_name"] = branch_detail['branch_name']
-                context["city"] = branch_detail['city']
-                context["bank_id"] = branch_detail['bank_id']
-                context["ifsc"] = branch_detail['ifsc']
-                context["address"] = branch_detail['address']
-                context["district"] = branch_detail['district']
-                context["state"] = branch_detail['state']
-                context["bank_name"] = branch_detail['bank_name']
+                context["branch_name"] = branch_detail.first().branch
+                context["city"] = branch_detail.first().city
+                context["bank_id"] = branch_detail.first().bank_id
+                context["ifsc"] = branch_detail.first().ifsc
+                context["address"] = branch_detail.first().address
+                context["district"] = branch_detail.first().district
+                context["state"] = branch_detail.first().state
+                context["bank_name"] = branch_detail.first().bank.name
                 return HttpResponse(json.dumps(context), status=200)
             else:
                 return HttpResponse('Not found', status=404)
         elif bank_name and bank_city:
-            # branch_detail = Branches.objects.filter(bank__name__iexact=bank_name, city__iexact=bank_city)
+            branch_detail = Branches.objects.filter(bank__name__iexact=bank_name, city__iexact=bank_city)
 
-            branch_detail = es.search(index="branch", doc_type='bank',  body={
-                "size" : 10000, "query": {"bool": {"must": [{"match_phrase": {"bank_name": bank_name}},{"match_phrase": {"city": bank_city}}]}}
-            })
-            branch_detail = branch_detail['hits']['hits']
-            if branch_detail:
+            if branch_detail.exists:
                 details = []
                 for i, branch in enumerate(branch_detail):
-                    branch = branch_detail[i]['_source']
                     context = {}
-                    context["branch_name"] = branch['branch_name']
-                    context["city"] = branch['city']
-                    context["bank_id"] = branch['bank_id']
-                    context["ifsc"] = branch['ifsc']
-                    context["address"] = branch['address']
-                    context["district"] = branch['district']
-                    context["state"] = branch['state']
-                    context["bank_name"] = branch['bank_name']
+                    context["branch_name"] = branch.branch
+                    context["city"] = branch.city
+                    context["bank_id"] = branch.bank_id
+                    context["ifsc"] = branch.ifsc
+                    context["address"] = branch.address
+                    context["district"] = branch.district
+                    context["state"] = branch.state
+                    context["bank_name"] = branch.bank.name
                     details.append(context)
                 return HttpResponse(json.dumps(details), status=200)
             else:
